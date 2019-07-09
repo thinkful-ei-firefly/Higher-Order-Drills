adda37a3facf6086e441bed0a13c6a52a72af271
Chris <50714992+cmpowers1981@users.noreply.github.com>
2019-07-09 12:37:56 -0400

    Chris update quest 2
    Question 2 of HOF drills

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+//question 1 - Matt
+'use strict';
 const repeat = (n, fn) => {
   for (let i = 0; i < n; i++) {
     fn(i);
@@ -13,3 +16,20 @@ const goodbye = () => console.log("Goodbye world");
 
 repeat(5, hello);
 repeat(5, goodbye);
+
+// Question 2
+
+function filter(array, fn) {
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+      fn(array[i]);
+      if (fn(array[i]) === true) arr.push(array[i])
+    }
+    return arr;
+}
+
+const myName = ['John', 'Paul', 'Ringo', 'George'];
+
+console.log(filter(myName, name => name[0] ==='G'));
+
+// Question 3
