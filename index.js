3668c98c01761b0fa159db38aa83581ab5af3135
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-09 12:01:32 -0400

    fixed code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const repeat = (n, fn){
-    for(let i =0; i < n; i++){
-        fn(n);
-    }
-}
+const repeat = (n, fn) => {
+  for (let i = 0; i < n; i++) {
+    fn(i);
+  }
+};
 
-repeat(5, (n)=>{n*5});
+repeat(5, n => {
+  console.log(n * 5);
+});
