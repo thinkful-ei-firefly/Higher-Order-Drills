44bca05c8df26e33b15a3ce21d088307788bfd6e
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-09 12:08:08 -0400

    added hello and goodbye

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const repeat = (n, fn) => {
 repeat(5, n => {
   console.log(n * 5);
 });
+
+const hello = () => console.log("Hello world");
+const goodbye = () => console.log("Goodbye world");
+
+repeat(5, hello);
+repeat(5, goodbye);
